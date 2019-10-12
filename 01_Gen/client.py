b74600a519a2edeeb8a76c4ddd62896bcf81375a
Victor Manuel Garcia Sanabria <vik000@gmail.com>
2019-10-13 00:55:31 +0100

    created db class and added client for generator

---
+++ 01_Gen/client.py
@@ -0,0 +1,21 @@
+import socket
+
+HEADERSIZE = 10
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(128)
+        if new_msg:
+            # print(f"new message length = {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+        if len(full_msg) - HEADERSIZE == msglen:
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
